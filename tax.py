d78ffe6f379c6f60e804dc80fb05f9604463f6e7
vikramagrwal2506 <pratik.udemylearn@gmail.com>
2025-07-30 08:53:58 +1000

    Update tax.py

--- tax.py
+++ tax.py
@@ -7,10 +7,10 @@ X2=(AI-135000)*(.37) + 31288
 X3=(AI-190000)*(.45) + 51638
 if AI <= 18200:
     print("Total Tax will be 0")
-    print("Medicare will be 0")
+    print("There will be no Medicare cost for the income mentioned.")
 elif AI<=45000:
     print ("Total Tax will be", X)
-    print("Medicare will be 0")
+    print("There will be no Medicare cost for the income mentioned.")
 elif AI<=135000:
     print ("Tax will be", X1)
 elif AI<=190000:
