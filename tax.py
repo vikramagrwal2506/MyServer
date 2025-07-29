e286c079f7fc5a26a1003358bff73d47adf92077
vikramagrwal2506 <pratik.udemylearn@gmail.com>
2025-07-29 23:37:34 +1000

    Create tax.py

---
+++ tax.py
@@ -0,0 +1,21 @@
+#Australia Income Tax calculator
+Annual_Income = (input("Annual_Income ="))
+AI=int(Annual_Income)
+X=(AI-18200)*(.16)
+X1=(AI-45000)*(.30) + 4288
+X2=(AI-135000)*(.37) + 31288
+X3=(AI-190000)*(.45) + 51638
+if AI <= 18200:
+    print("Total Tax will be 0")
+    print("Medicare will be 0")
+elif AI<=45000:
+    print ("Total Tax will be", X)
+    print("Medicare will be 0")
+elif AI<=135000:
+    print ("Tax will be", X1)
+elif AI<=190000:
+    print ("Tax will be", X2)
+elif AI>190000:
+    print ("Tax will be", X3)
+else:
+    print("ERROR")
